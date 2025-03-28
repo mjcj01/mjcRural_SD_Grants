14324d7ee3c90f80f1910e5cb6b160fd7110db48
Michael Cattell <mjclego@icloud.com>
2025-03-28 15:42:12 -0400

    Small edits

--- Scripts/data_load.R
+++ Scripts/data_load.R
@@ -3,6 +3,26 @@ library(educationdata)
 
 revenue_22_23 <- read_csv("Data//revenue_22_23.csv")
 
+revenue_overview_22_23 <- read_csv("Data//revenue_overview_22_23.csv")
+
+colnames(revenue_overview_22_23) <- c("AUN",
+                                      "school_district",
+                                      "county",
+                                      "tot_rev",
+                                      "loc_tax_rev",
+                                      "loc_oth_rev",
+                                      "loc_tot_rev",
+                                      "sta_tot_rev",
+                                      "fed_tot_rev",
+                                      "oth_tot_rev")
+
+adm_22_23 <- read_csv("Data//adm_22_23.csv")
+
+colnames(adm_22_23) <- c("AUN", "school_district", "county", "adm", "wadm")
+
+adm_22_23 <- adm_22_23 %>%
+  select("AUN", "adm", "wadm")
+
 # pa_directory <- get_education_data(level = 'school-districts',
 #                                    source = 'ccd',
 #                                    topic = 'directory',
@@ -38,3 +58,8 @@ rural_school_enrollment <- pa_enrollment %>%
 
 rural_school_spending <- revenue_22_23 %>%
   filter(AUN %in% rural_school_directory$state_leaid)
+
+nces_locale_classifications <- tibble("Rural Classification" = c("Fringe", "Distant", "Remote"),
+                                      "Criteria" = c("5 miles or closer to urban area with 50,000 or more people, and is 2.5 miles or closer to urban area with less than 50,000 people.",
+                                                     "5 - 25 miles from an urban area with 50,000 or more people, and is 2.5 - 10 miles from an urban area with less than 50,000 people.",
+                                                     "25 miles from an urban area with 50,000 or more people, and more than 10 miles from an urban area with less than 50,000 people."))
